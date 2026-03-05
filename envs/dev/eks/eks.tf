c032140e9d227948e8c618716a335d1b0e90ba25
Chinmaya <chinmayapradhan10000@gmail.com>
2026-03-05 11:25:38 +0530

    configure eks

--- envs/dev/eks/eks.tf
+++ envs/dev/eks/eks.tf
@@ -1,78 +1,43 @@
- module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
-
-  name               = "${var.env}-${var.eks_cluster_name}"
-  kubernetes_version = var.eks_version
-
-  addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {
-      before_compute = true
-    }
-    kube-proxy             = {}
-    vpc-cni                = {
-      before_compute = true
-    }
-  }
-
-  # Optional
-  endpoint_public_access = true
-
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
-  enable_cluster_creator_admin_permissions = true
-
-  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subet_ids
-#   control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_groups = {
-    initial = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node group
-      instance_types = var.general_nodes_ec2_types
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
-    }
-  }
-
-  tags = {
-    Environment = var.env
-    Terraform   = "true"
-  }
+resource "aws_iam_role" "cluster" {
+  name = "${var.env}-${var.eks_cluster_name}-eks-cluster"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
 
-module "eks_blueprints_addons" {
-  source = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0" #ensure to update this to the latest/desired version
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
 
-  cluster_name      = module.eks.cluster_name
-  cluster_endpoint  = module.eks.cluster_endpoint
-  cluster_version   = module.eks.cluster_version
-  oidc_provider_arn = module.eks.oidc_provider_arn
+resource "aws_eks_cluster" "eks" {
+  name     = "${var.env}-${var.eks_cluster_name}"
+  version  = var.eks_version
+  role_arn = aws_iam_role.cluster.arn
 
-  # enable_aws_load_balancer_controller    = true
-  #enable_cluster_proportional_autoscaler = true
-  #enable_karpenter                       = true
-  #enable_kube_prometheus_stack           = true
-  enable_metrics_server                  = true
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
 
-  enable_cluster_autoscaler           = true
-  cluster_autoscaler = {
-    set = [
-      {
-        name  = "extraArgs.scale-down-unneeded-time"
-        value = "1m"
-      },
-      {
-        name  = "extraArgs.skip-nodes-with-local-storage"
-        value = "false"
-      },
-      {
-        name  = "extraArgs.skip-nodes-with-system-pods"
-        value = "false"
-      }
-    ]
+    subnet_ids              = data.terraform_remote_state.vpc.outputs.private_subet_ids
   }
+
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks]
 }
--- envs/dev/eks/providers.tf
+++ envs/dev/eks/providers.tf
@@ -17,27 +17,17 @@ terraform {
   }
 }
 
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+  load_config_file       = false
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
