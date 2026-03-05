5acd6e78f057401ab41ce10a90e1e6c8ed200b21
Chinmaya <chinmayapradhan10000@gmail.com>
2026-03-05 16:28:52 +0530

    change subnet name

--- envs/dev/vpc/main.tf
+++ envs/dev/vpc/main.tf
@@ -34,12 +34,12 @@ module "vpc" {
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb"                          = 1
-    "kubernetes.io/cluster/${var.env}-${var.eks_cluster_name}" = "owned"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "owned"
   }
 
   public_subnet_tags = {
     "kubernetes.io/role/elb"                                   = 1
-    "kubernetes.io/cluster/${var.env}-${var.eks_cluster_name}" = "owned"
+    "kubernetes.io/cluster/${var.eks_cluster_name}" = "owned"
   }
 
   tags = {
